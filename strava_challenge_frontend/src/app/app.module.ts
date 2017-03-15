e0210f9628053f018976098a624e8d450d8172de
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2017-03-15 22:16:12 +0100

    Add structure to landing page

--- strava_challenge_frontend/src/app/app.module.ts
+++ strava_challenge_frontend/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { LandingPageModule } from './landing-page/landing-page.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    LandingPageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
